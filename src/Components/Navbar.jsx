0e92b4415cdc05d83c8f27d980f5f10d633b14e2
Kavya <edigakavya369@gmail.com>
2026-04-16 15:34:12 +0530

    logo to home page navigation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-3">
 
         {/* Logo Section */}
-        <div className="flex items-center space-x-3">
+        <NavLink to="/" className="flex items-center space-x-3">
           <img
             src={logo}
             alt="VAYKR Academy"
@@ -34,7 +34,7 @@ export default function Navbar() {
               Success is not Destination, It’s a Journey
             </p>
           </div>
-        </div>
+        </NavLink>
 
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-6">
